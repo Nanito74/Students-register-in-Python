39424b66fe341e6e83bf16d37a737660aa0036fc
CristianDalpino <cristiandalpinogs@gmail.com>
2021-10-20 21:31:42 -0300

    Condicion del Alumno

--- alumno.py
+++ alumno.py
@@ -10,3 +10,16 @@ class Alumno:
         self.p2 = p2
         self.condicion = condicion
         self.notafinal = notafinal
+
+#Este metodo lo crearemos para que el alumno cambie la condición automáticamente al saber sus notas y asistencia.
+#Si falta alguna nota dejaremos su condicion como incompleto
+ 
+def cambiar_condicion(self):
+        if self.asistencia is None or self.tp is None or self.p is None or self.p1 is None or self.p2 is None:
+            self.condicion = 'Incompleto' 
+        elif int(self.asistencia) >= 75 and int(self.tp) < 100 and int(self.p1) >= 8 and int(self.p2) >= 8:
+            self.condicion = 'Aprobado'
+        elif int(self.asistencia) >= 75 and int(self.tp) < 100 and int(self.p1) >= 6 or int(self.p2) >= 6:
+            self.condicion = 'Regular'
+        else:
+            self.condicion = 'Desaprobado'
