fe8bc2f78f49e6f847c5312fa90ec1a27dda715e
Nanito74 <marianocordoba00@gmail.com>
2021-10-22 18:27:31 -0300

    Tabla de registro

--- menu.py
+++ menu.py
@@ -31,3 +31,26 @@ class Menu:
         self.b6 = ttk.Button(self.ventana, text ="Buscar por condicion",command=lambda:[self.limpiarmarcos(self.marco6)])
         self.b6.grid(row=0,column=5)
         self.b7 = ttk.Button(self.ventana, text ="Salir",command=self.salir).grid(row=0,column=6)
+
+#Aca disenaremos la tabla, la cual usaremos para mostrar el registro completo, o con las condiciones correspondientes
+        self.marco1 = LabelFrame(self.ventana,text='Registro')
+        self.marco1.grid(row=2,column=0,columnspan=1000)
+        self.marco1.grid_remove()
+        
+        self.tabla = ttk.Treeview(self.marco1,columns= ('c1', 'c2','c3', 'c4', 'c5', 'c6'))
+        self.tabla.column('#0',anchor=CENTER)
+        self.tabla.column('c1',anchor=CENTER)
+        self.tabla.column('c2',anchor=CENTER)
+        self.tabla.column('c3',anchor=CENTER)
+        self.tabla.column('c4',anchor=CENTER)
+        self.tabla.column('c5',anchor=CENTER)
+        self.tabla.column('c6',anchor=CENTER)
+        self.tabla.grid(row=2,column=0)
+       
+        self.tabla.heading('#0',text='DNI')
+        self.tabla.heading('c1',text='Nombre')
+        self.tabla.heading('c2',text='Asistencia')
+        self.tabla.heading('c3',text='Tps')
+        self.tabla.heading('c4',text='Parcial')
+        self.tabla.heading('c5',text='Condicion')
+        self.tabla.heading('c6',text='Libre')
